27151ef15227f86a952568a1165efef7f63ca7ce
csf24mun <nmullen123@icloud.com>
2025-10-06 15:00:13 +0100

    Commit4

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Library;
+using Week3;
 class Program
 {
     static void Main(string[] args)
@@ -6,7 +7,19 @@ class Program
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
+        Book book1 = new Book("Ultimate C#", "Microsoft", "2233445");
+        Console.WriteLine("Currently available books -------------------\n");
         book.DisplayInfo();
+        book1.DisplayInfo();
+
+        // Create new instances of the Member class
+        // These new members are created using the
+        // Member constructor in the Members class
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members -------------------\n");
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
