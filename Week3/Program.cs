8700497b45a4417080e6247afe636a1c59b6e5bd
csf24mun <nmullen123@icloud.com>
2025-10-06 14:41:15 +0100

    Commit2

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,28 +1,6 @@
-﻿class Book
+﻿using Library;
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    // Example of a constructor that allows us to 'construct' a new
-    // Book object
-    //Test
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-
-
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-
     static void Main(string[] args)
     {
         // Create a new instance (object) of the Book class
@@ -31,28 +9,4 @@
 
         book.DisplayInfo();
     }
-
-    /*static void Main(string[] args)
-    {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
-        Book book = new Book();
-
-        // This information is for one book in our library
-        book.Title = "C# for beginners";
-        book.Author = "BillGates";
-        book.ISBN = "12345678";
-
-        // This is another book object.
-        // Note that the object name is 'book1' and not 'book'
-        Book book1 = new Book();
-        book1.Title = "C# Methods and classes";
-        book1.Author = "Microsoft";
-        book1.ISBN = "55667778";
-
-        // Output book information to the console
-        book.DisplayInfo();
-        book1.DisplayInfo();
-    }*/
 }
-
